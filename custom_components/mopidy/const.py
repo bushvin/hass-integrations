8ce2ea63f1ebd97bb6904c44fb4a6efe94b21f23
schmacka <sebastian@seubertonline.de>
2021-03-17 19:49:47 +0000

    Forgot to include folder

---
+++ custom_components/mopidy/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Mopidy integration."""
+
+DOMAIN = "mopidy"
+ICON = "mdi:music-note"
+DEFAULT_NAME = "Mopidy"
+DEFAULT_PORT = 6680
+SERVICE_SNAPSHOT = "snapshot"
+SERVICE_RESTORE = "restore"
+CACHE_TITLES = {}
+CACHE_ART = {}
